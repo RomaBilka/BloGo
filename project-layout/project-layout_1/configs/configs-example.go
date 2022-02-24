26c99dcbe3898910936377a5a2b2070215486d84
Roman Biliak <roman_biliak@epam.com>
2022-02-24 12:17:25 +0200

    config

---
+++ project-layout/project-layout_1/configs/configs-example.go
@@ -0,0 +1,6 @@
+package configs
+
+const PG_USER = ""
+const PG_PASSWORD = ""
+const PG_DATABASE = ""
+const PG_HOST = ""
--- project-layout/project-layout_2/configs/configs.go
+++ project-layout/project-layout_2/configs/configs.go
@@ -1,6 +1,6 @@
 package configs
 
-const PG_USER = ""
-const PG_PASSWORD = ""
-const PG_DATABASE = ""
-const PG_HOST = ""
+const PG_USER = "postgres"
+const PG_PASSWORD = "secret"
+const PG_DATABASE = "example-db"
+const PG_HOST = "127.0.0.1"
