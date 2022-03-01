e8be5683ea2eb375580fcf8886e9839ecb1adfa3
Roman Biliak <roman_biliak@epam.com>
2022-03-01 20:13:53 +0200

    userRepository integration test

---
+++ tests/internal/repositories/main_test.go
@@ -0,0 +1,74 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/RomaBiliak/BloGo/project-layout/tests/internal/models"
+	"github.com/RomaBiliak/BloGo/project-layout/tests/pkg/database/postgres"
+	"github.com/bxcodec/faker/v3"
+	"github.com/joho/godotenv"
+)
+
+var testUserRepository *UserRepository
+var db *sql.DB
+
+type test struct {
+	Name string `faker:"name"`
+	Email string `faker:"email"`
+	Phone string `faker:"e_164_phone_number"`
+}
+var testData test
+
+var userTest models.User
+
+func TestMain(m *testing.M) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	err = faker.FakeData(&userTest)
+	if err != nil {
+		panic(err)
+	}
+
+	userTest = models.User{Name: userTest.Name, Email: userTest.Email, Phone: userTest.Phone}
+
+	pgUser, ok := os.LookupEnv("PG_USER")
+	if !ok {
+		panic(errors.New("PG_USER is empty"))
+	}
+	pgPassword, ok := os.LookupEnv("PG_PASSWORD")
+	if !ok {
+		panic(errors.New("PG_PASSWORD is empty"))
+	}
+	pgDatabase, ok := os.LookupEnv("PG_TEST_DATABASE")
+	if !ok {
+		panic(errors.New("PG_TEST_DATABASE is empty"))
+	}
+	pgHost, ok := os.LookupEnv("PG_HOST")
+	if !ok {
+		panic(errors.New("Db Host is empty"))
+	}
+
+	dbConfig := postgres.Config{
+		pgUser,
+		pgPassword,
+		pgDatabase,
+		pgHost,
+	}
+	db = postgres.Run(dbConfig)
+
+	defer db.Close()
+	testUserRepository = NewPostgreUserRepository(db)
+
+	os.Exit(m.Run())
+}
+
+func truncateUsers() error {
+	_, err := db.Query("TRUNCATE users CASCADE")
+	return err
+}
---
+++ tests/internal/repositories/userRepository_test.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"testing"
+
+	"github.com/RomaBiliak/BloGo/project-layout/tests/internal/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func createUser(t *testing.T) models.UserId {
+	id, err := testUserRepository.CreateUser(userTest)
+	assert.NotNil(t, id)
+	assert.NoError(t, err)
+	return id
+}
+
+func TestCreateUser(t *testing.T) {
+	defer func() {
+		err := truncateUsers()
+		assert.NoError(t, err)
+	}()
+	createUser(t)
+}
+
+func TestGetUserById(t *testing.T) {
+	defer func() {
+		err := truncateUsers()
+		assert.NoError(t, err)
+	}()
+	id := createUser(t)
+
+	u, err := testUserRepository.GetUserById(id)
+	assert.Equal(t, u.Id, id)
+	assert.NoError(t, err)
+}
+
+
+func TestCheckUserExistsTrue(t *testing.T) {
+	defer func() {
+		err := truncateUsers()
+		assert.NoError(t, err)
+	}()
+	createUser(t)
+
+	ok, err := testUserRepository.CheckUserExists(userTest)
+
+	assert.True(t, ok)
+	assert.NoError(t, err)
+}
